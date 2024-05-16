cc86a7db52b2ca72d0772262c6439c629957a595
Konnen123 <73136383+Konnen123@users.noreply.github.com>
2024-05-16 22:11:41 +0300

    Hotfix: Refactor login to use postgres api

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,4 +1,4 @@
-const dbConnection = require('../config/database')
+const dbConnection = require('../config/postgresDB')
 
 class UserService{
     async add(email, name, password, isAdmin){
@@ -6,11 +6,12 @@ class UserService{
     }
 
     static async addUser(email, name, password, isAdmin){
+        const insertQuery = {
+            text: 'INSERT INTO sql_tutoring."User" (email, name, password, isAdmin) VALUES ($1, $2, $3, $4)',
+            values: [email, name, password, isAdmin],
+          };
         try {
-            await dbConnection.query(
-                'INSERT INTO User (email, name, password, isAdmin) VALUES (?, ?, ?, ?)',
-                [email, name, password, isAdmin]
-            );
+            await dbConnection.query(insertQuery);
             console.log('User inserted successfully:');
         } catch (error) {
             console.error('Error inserting user:', error.message);
@@ -18,11 +19,13 @@ class UserService{
     }
     static async getUser(email)
     {
+        const selectQuery = {
+            text: 'SELECT * FROM sql_tutoring."User" WHERE email = $1',
+            values: [email],
+          };
         try {
-            const result = await dbConnection.query(
-                'SELECT * FROM User WHERE email = ?', email
-            );
-            return result;
+            const result = await dbConnection.query(selectQuery);
+            return result.rows;
 
         } catch (error) {
             console.error('Error selecting user:', error.message);
